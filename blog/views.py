52ba82f74db1633c36a750228c60d4a5a6b64b24
jonathandelimas <johnlimma@gmail.com>
2022-09-29 16:58:27 +1000

    First Commit

---
+++ blog/views.py
@@ -0,0 +1,18 @@
+from multiprocessing import context
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title':'About' })
+
+
+# Create your views here.

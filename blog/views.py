029b72adfe394bd14ac9c098f7d98727296d2b11
jonathandelimas <johnlimma@gmail.com>
2022-10-07 00:30:44 +1100

    updates07.10

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
@@ -16,6 +16,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html'    #<app>/<model>_<viewtype>.html
     context_object_name= 'posts'
     ordering = ['-date_posted']
+    paginate_by = 2
     
 class PostDetailView(DetailView):
     model = Post
@@ -35,6 +36,16 @@ class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = '/'
     
     def test_func(self):
         post = self.get_object()
